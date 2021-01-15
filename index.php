0b5ec50e788bd0071a2405ada3b56683da604588
Ale <alessandrochb@gmail.com>
2021-01-15 15:39:15 +0100

    stampati in pagina i due oggetti con html

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo 'hello';
-echo '<br>';
+// echo 'hello';
+// echo '<br>';
 
 class Movie {
 
@@ -22,7 +22,7 @@ class Movie {
 };
 
 // CREO UN PRIMO OGGETTO
-$firstFilm = new Movie('amici miei', '9', 'comico');
+$firstFilm = new Movie('Amici miei', '9', 'Comico');
 
 $firstMovieTitle = $firstFilm->title;
 
@@ -32,18 +32,18 @@ $firstMovieYear = $firstFilm->year = 1975;
 
 $firstMovieGenre = $firstFilm->getGenre();
 
-echo($firstMovieTitle);
-echo '<br>';
-echo($firstMovieDirector);
-echo '<br>';
-echo($firstMovieYear);
-echo '<br>';
-echo($firstMovieGenre);
-echo '<br>';
+// echo($firstMovieTitle);
+// echo '<br>';
+// echo($firstMovieDirector);
+// echo '<br>';
+// echo($firstMovieYear);
+// echo '<br>';
+// echo($firstMovieGenre);
+// echo '<br>';
 
 
 // CREO UN SECONDO OGGETTO
-$secondFilm = new Movie('interstellar', '8.5', 'fantascienza');
+$secondFilm = new Movie('Interstellar', '8.5', 'Fantascienza');
 
 $secondMovieTitle = $secondFilm->title;
 
@@ -53,12 +53,45 @@ $secondMovieYear = $secondFilm->year = 2014;
 
 $secondMovieGenre = $secondFilm->getGenre();
 
-echo($secondMovieTitle);
-echo '<br>';
-echo ($secondMovieDirector);
-echo '<br>';
-echo ($secondMovieYear);
-echo '<br>';
-echo ($secondMovieGenre);
+// echo($secondMovieTitle);
+// echo '<br>';
+// echo ($secondMovieDirector);
+// echo '<br>';
+// echo ($secondMovieYear);
+// echo '<br>';
+// echo ($secondMovieGenre);
 
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>First Class</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1 class="text-uppercase">firt class</h1>
+        </header>
+        <main>
+            <div class="container">
+                <div class="card-container">
+                    <div class="card">
+                        <h2>First Movie</h2>
+                        <p><?php echo $firstMovieTitle?></p>
+                        <p><?php echo $firstMovieDirector?></p>
+                        <p><?php echo $firstMovieYear?></p>
+                        <p><?php echo $firstMovieGenre?></p>
+                    </div>
+                    <div class="card">
+                        <h2>Second Movie</h2>
+                        <p><?php echo $secondMovieTitle?></p>
+                        <p><?php echo $secondMovieDirector?></p>
+                        <p><?php echo $secondMovieYear?></p>
+                        <p><?php echo $secondMovieGenre?></p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
